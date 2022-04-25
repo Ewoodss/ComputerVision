2c1661e151af0806422d332c6c9611377f25c300
E_Woods <we.baars@student.avans.nl>
2022-04-25 16:07:39 +0200

    Chapter 4

--- ComputerVision/Chapter4.cpp
+++ ComputerVision/Chapter4.cpp
@@ -8,16 +8,20 @@ using namespace std;
 
 //////////////  Draw Shapes and Text //////////////////////
 
-void main() {
-
+void main()
+{
 	// Blank Image 
 	Mat img(512, 512, CV_8UC3, Scalar(255, 255, 255));
 
-	circle(img, Point(256, 256), 155, Scalar(0, 69, 255), FILLED);
-	rectangle(img, Point(130, 226), Point(382, 286), Scalar(255, 255, 255), FILLED);
-	line(img, Point(130, 296), Point(382, 296), Scalar(255, 255, 255), 2);
+	circle(img, Point(128, 128), 120, Scalar(0, 69, 255), 8);
+	circle(img, Point(384, 128), 120, Scalar(0, 69, 255), 8);
 
-	putText(img, "Murtaza's Workshop", Point(137, 262), FONT_HERSHEY_DUPLEX, 0.75, Scalar(0, 69, 255), 2);
+
+	rectangle(img, Point(128, 384 + 64), Point(128 * 3, 256 + 64), Scalar(0, 69, 255), LINE_AA);
+
+	line(img, Point(128, 384), Point(128 * 3, 384), Scalar(0, 69, 255), 2);
+
+	putText(img, "~~~~Snor~~~~", Point(160, 280), FONT_HERSHEY_DUPLEX, 0.75, Scalar(0, 69, 255), 2);
 
 	imshow("Image", img);
 	waitKey(0);
--- ComputerVision/Chapter5.cpp
+++ ComputerVision/Chapter5.cpp
@@ -8,16 +8,16 @@ using namespace std;
 
 ///////////////  Warp Images  //////////////////////
 
-void main() {
-
+void main()
+{
 	string path = "Resources/TheFutureOfEnergy3.jpg";
 	Mat img = imread(path);
 
 	Mat matrix, imgWarp;
-	float w = 210*3, h = 297*3;
+	float w = 210 * 3, h = 297 * 3;
 
-	Point2f src[4] = { {1665, 159},{3022, 177},{1465, 2118},{3311, 2104} };
-	Point2f dst[4] = { {0.0f,0.0f},{w,0.0f},{0.0f,h},{w,h} };
+	Point2f src[4] = {{1665, 159}, {3022, 177}, {1465, 2118}, {3311, 2104}};
+	Point2f dst[4] = {{0.0f, 0.0f}, {w, 0.0f}, {0.0f, h}, {w, h}};
 
 	matrix = getPerspectiveTransform(src, dst);
 	warpPerspective(img, imgWarp, matrix, Point(w, h));
@@ -33,4 +33,3 @@ void main() {
 	imshow("Image Warp", imgWarp);
 	waitKey(0);
 }
-
