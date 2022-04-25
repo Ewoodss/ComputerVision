f34705fdaee4c8a82dfc1f1664e87034b5013b7b
Luuk Verhagen <l.verhagen5@student.avans.nl>
2022-04-25 15:06:30 +0200

    Edit chapter 3

--- ComputerVision/Chapter3.cpp
+++ ComputerVision/Chapter3.cpp
@@ -11,9 +11,12 @@ using namespace std;
 
 void main() {
 
-	string path = "Resources/test.png";
-	Mat img = imread(path);
+	//string path = "Resources/test.png";
+	VideoCapture cap(0);
+	Mat img;
 	Mat imgResize, imgCrop;
+
+	cap.read(img);
 
 	//cout << img.size() << endl;
 	resize(img, imgResize, Size(), 0.5, 0.5);
