4bde2efbb0c8015092e804702de859fe0014e8a8
Luuk Verhagen <l.verhagen5@student.avans.nl>
2022-05-26 14:21:19 +0200

    Started with eindopdracht

--- ComputerVision/eindopdracht.cpp
+++ ComputerVision/eindopdracht.cpp
@@ -8,10 +8,13 @@ using namespace std;
 
 ///////////////  Project 3 - License Plate Detector //////////////////////
 
-void main() {
+void preProcessPlate(Mat &img, Mat &resultImg, int i = 0);
+void getContours(Mat imgDil, Mat &img);
 
-	Mat img;
-	VideoCapture cap(0);
+int cannyThreshold = 25;
+
+void main() {
+	Mat img = imread("Resources/KentekensRU.jpg");
 
 	CascadeClassifier plateCascade;
 	plateCascade.load("Resources/haarcascade_russian_plate_number.xml");
@@ -20,20 +23,77 @@ void main() {
 
 	vector<Rect> plates;
 
-	while (true) {
-
-		cap.read(img);
+	while (true)
+	{
 		plateCascade.detectMultiScale(img, plates, 1.1, 10);
 
 		for (int i = 0; i < plates.size(); i++)
 		{
-			Mat imgCrop = img(plates[i]);
-			//imshow(to_string(i), imgCrop);
-			imwrite("/Resources/Plates/" + to_string(i) + ".png", imgCrop);
-			rectangle(img, plates[i].tl(), plates[i].br(), Scalar(255, 0, 255), 3);
+			Mat plateImgOriginal = img(plates[i]);
+			Mat plateImgResult;
+			preProcessPlate(plateImgOriginal, plateImgResult, i);
+			getContours(plateImgResult, plateImgOriginal);
+
+			imshow(to_string(i), plateImgOriginal);
+			imwrite("/Resources/Plates/" + to_string(i) + ".png", plateImgOriginal);
+			//rectangle(img, plates[i].tl(), plates[i].br(), Scalar(255, 0, 255), 3);
 		}
 
 		imshow("Image", img);
-		waitKey(1);
+		waitKey(1000);
 	}
+}
+
+void getContours(Mat imgDil, Mat &img) {
+
+	vector<vector<Point>> contours;
+	vector<Vec4i> hierarchy;
+
+	findContours(imgDil, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+	//drawContours(img, contours, -1, Scalar(255, 0, 255), 2);
+
+	vector<vector<Point>> conPoly(contours.size());
+	vector<Rect> boundRect(contours.size());
+
+	for (int i = 0; i < contours.size(); i++)
+	{
+		int area = contourArea(contours[i]);
+		cout << area << endl;
+		string objectType;
+
+		if (area > 10000)
+		{
+			float peri = arcLength(contours[i], true);
+			approxPolyDP(contours[i], conPoly[i], 0.02 * peri, true);
+			cout << conPoly[i].size() << endl;
+			boundRect[i] = boundingRect(conPoly[i]);
+
+			int objCor = (int)conPoly[i].size();
+
+			if (objCor == 3) { objectType = "Tri"; }
+			else if (objCor == 4)
+			{
+				float aspRatio = (float)boundRect[i].width / (float)boundRect[i].height;
+				cout << aspRatio << endl;
+				if (aspRatio > 0.95 && aspRatio < 1.05) { objectType = "Square"; }
+				else { objectType = "Rect"; }
+			}
+			else if (objCor > 4) { objectType = "Circle"; }
+
+			drawContours(img, conPoly, i, Scalar(255, 0, 255), 2);
+			rectangle(img, boundRect[i].tl(), boundRect[i].br(), Scalar(0, 255, 0), 5);
+			putText(img, objectType, { boundRect[i].x,boundRect[i].y - 5 }, FONT_HERSHEY_PLAIN, 1, Scalar(0, 69, 255), 2);
+		}
+	}
+}
+
+void preProcessPlate(Mat &img, Mat &resultImg, int i) {
+
+	Mat imgGray, imgBlur, imgCanny, imgDil, imgErode;
+	cvtColor(img, imgGray, COLOR_BGR2GRAY);
+	GaussianBlur(imgGray, imgBlur, Size(3, 3), 3, 0);
+	Canny(imgBlur, imgCanny, cannyThreshold, cannyThreshold*3);
+	//imshow(to_string(i+10), imgCanny);
+	Mat kernel = getStructuringElement(MORPH_RECT, Size(3, 3));
+	dilate(imgCanny, resultImg, kernel);
 }
