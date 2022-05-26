1bfbd0904abcfe57fc4aea5c7d8cb2fa85f757be
Luuk Verhagen <l.verhagen5@student.avans.nl>
2022-05-26 13:25:56 +0200

    Added chapter 6 and 7

--- ComputerVision/Chapter6.cpp
+++ ComputerVision/Chapter6.cpp
@@ -10,17 +10,20 @@ using namespace std;
 
 void main() {
 
-	string path = "Resources/lambo.png";
+	string path = "Resources/kerstballen.bmp";
 	Mat img = imread(path);
 	Mat imgHSV, mask;
 	int hmin = 0, smin = 110, vmin = 153;
 	int hmax = 19, smax = 240, vmax = 255;
+	int hue = 0, hueRange;
 
 	cvtColor(img, imgHSV, COLOR_BGR2HSV);
 
 	namedWindow("Trackbars", (640, 200));
-	createTrackbar("Hue Min", "Trackbars", &hmin, 179);
-	createTrackbar("Hue Max", "Trackbars", &hmax, 179);
+	createTrackbar("Hue", "Trackbars", &hue, 360);
+	createTrackbar("Hue range", "Trackbars", &hueRange, 179);
+	//createTrackbar("Hue Min", "Trackbars", &hmin, 179);
+	//createTrackbar("Hue Max", "Trackbars", &hmax, 179);
 	createTrackbar("Sat Min", "Trackbars", &smin, 255);
 	createTrackbar("Sat Max", "Trackbars", &smax, 255);
 	createTrackbar("Val Min", "Trackbars", &vmin, 255);
@@ -28,9 +31,9 @@ void main() {
 
 	while (true) {
 
-		Scalar lower(hmin, smin, vmin);
-		Scalar upper(hmax, smax, vmax);
-		inRange(imgHSV, lower, upper, mask);
+		Scalar lower(hue - 0.5 * hueRange, smin, vmin);
+		Scalar upper(hue + 0.5 * hueRange, smax, vmax);
+		inRange(img, lower, upper, mask);
 
 		imshow("Image", img);
 		imshow("Image HSV", imgHSV);
